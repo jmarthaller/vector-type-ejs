771c20913f136748525753575f4fd7b5edccc59f
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-06 08:16:52 -0700

    implement new strategy

--- index.js
+++ index.js
@@ -47,3 +47,16 @@ class VillageState {
     }
   }
 }
+
+let first = new VillageState(
+  "Post Office",
+  [{place: "Post Office", address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+// → Alice's House
+console.log(next.parcels);
+// → []
+console.log(first.place);
+// → Post Office

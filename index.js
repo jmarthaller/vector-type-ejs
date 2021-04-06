40c480489d44132a6881a1ffd72fff455193b6fd
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-06 08:05:28 -0700

    format graph

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-class Vector {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-    }
-  
-    plus(other) {
-      return new Vec(this.x + other.x, this.y + other.y);
-    }
-  
-    minus(other) {
-      return new Vec(this.x - other.x, this.y - other.y);
-    }
-  
-    get length() {
-      return Math.sqrt(this.x * this.x + this.y * this.y);
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
     }
   }
-  
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
